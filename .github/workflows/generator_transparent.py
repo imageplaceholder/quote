a754966d30a07dd816adfe94e7260560bc4cdbd5
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-09-07 00:48:17 -0400

    Update generator_transparent.py

--- .github/workflows/generator_transparent.py
+++ .github/workflows/generator_transparent.py
@@ -5,7 +5,7 @@ from html2image import Html2Image
 hti = Html2Image(size=(780, 740),  custom_flags=['--virtual-time-budget=200'])
 
 
-hti.browser.flags = ['--default-background-color=0', '--hide-scrollbars']
+hti.browser.flags = ['--default-background-color=0', '--hide-scrollbars', '--virtual-time-budget=200']
 
 
 tags = ["business", "change", "character", "competition", "conservative", "courage", "education", "faith", "family", "famous-quotes", "film", "freedom", "friendship", "future", "happiness", "history", "honor", "humor", "humorous", "inspirational", "leadership", "life", "literature", "love", "motivational", "nature", "pain", "philosophy", "politics", "power-quotes", "religion", "science", "self", "self-help", "social-justice", "spirituality", "sports", "success", "technology", "time", "truth", "virtue", "war", "wisdom"]
