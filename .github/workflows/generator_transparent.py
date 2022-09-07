e8478abd78ba095e67ede6cbc779cd265182dbad
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-09-07 00:44:31 -0400

    Update generator_transparent.py

--- .github/workflows/generator_transparent.py
+++ .github/workflows/generator_transparent.py
@@ -2,8 +2,9 @@ import time
 import concurrent.futures
 
 from html2image import Html2Image
-hti = Html2Image(size=(780, 740),  custom_flags=['--virtual-time-budget=200', '--hide-scrollbars', '--default-background-color=00000000'])
+hti = Html2Image(size=(780, 740))
 
+hti.browser.flags = ['--virtual-time-budget=200', '--hide-scrollbars', '--default-background-color=00000000']
 
 tags = ["business", "change", "character", "competition", "conservative", "courage", "education", "faith", "family", "famous-quotes", "film", "freedom", "friendship", "future", "happiness", "history", "honor", "humor", "humorous", "inspirational", "leadership", "life", "literature", "love", "motivational", "nature", "pain", "philosophy", "politics", "power-quotes", "religion", "science", "self", "self-help", "social-justice", "spirituality", "sports", "success", "technology", "time", "truth", "virtue", "war", "wisdom"]
 
